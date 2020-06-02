ca233b61d63f0f2141f142538f850fb5e20a3d9d
Luis <luis@lelc.com.br>
2020-06-02 19:56:10 +0200

    realizando dia 2

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,12 @@
-import express, { response } from "express";
+import express, { response, request } from "express";
+import path from "path";
+import routes from "./routes";
 
 const app = express();
 
-app.get("/users", (request, response) => {
-  console.log("listagem de users");
+app.use(express.json());
+app.use(routes);
 
-  response.json(["1", "2", "3", "4"]);
-});
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.listen(3333);
